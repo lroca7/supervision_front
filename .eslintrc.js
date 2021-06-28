8d7d0a13842f3fcfec8f507d671e946d93241c7f
lrodriguez <lizethcabrales@gmail.com>
2021-06-28 18:56:09 -0500

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,9 +69,9 @@ module.exports = {
     'no-var': 'error',
     'object-shorthand': 'error',
     'prefer-const': 'error',
-    'prefer-template': 'error',
+    'prefer-template': 'error'
 
-    'parser': 'babel-eslint'
+    // 'parser': 'babel-eslint'
   }
 
   // rules: {
