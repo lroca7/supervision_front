4c899241666a27532e34ba5012657427287eb4dd
James Lotero Bedoya <jloterobedoya@gmail.com>
2021-09-28 14:16:05 -0500

    despliegue app qa

--- src/contants.js
+++ src/contants.js
@@ -1,5 +1,5 @@
 //Ambiente desa
-export const URL_BACK = 'https://zaemfz4o3j.execute-api.us-east-1.amazonaws.com/desa/desa-services_sync/'
+//export const URL_BACK = 'https://zaemfz4o3j.execute-api.us-east-1.amazonaws.com/desa/desa-services_sync/'
 
 //Ambiente bvrd-qa
-//export const URL_BACK = 'https://sk76wd183f.execute-api.us-east-1.amazonaws.com/bvrd-qa/services_sync/'
+export const URL_BACK = 'https://sk76wd183f.execute-api.us-east-1.amazonaws.com/bvrd-qa/services_sync/'
